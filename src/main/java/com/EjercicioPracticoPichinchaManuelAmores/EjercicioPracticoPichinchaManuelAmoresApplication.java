ee06abf6e22b6bb2f2013984df5835253547807a
manuel-amores <manuel.amores@globant.com>
2023-09-04 15:27:33 -0600

    first commit

---
+++ src/main/java/com/EjercicioPracticoPichinchaManuelAmores/EjercicioPracticoPichinchaManuelAmoresApplication.java
@@ -0,0 +1,13 @@
+package com.EjercicioPracticoPichinchaManuelAmores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioPracticoPichinchaManuelAmoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioPracticoPichinchaManuelAmoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EjercicioPracticoPichinchaManuelAmores/EjercicioPracticoPichinchaManuelAmoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EjercicioPracticoPichinchaManuelAmores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPracticoPichinchaManuelAmoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
